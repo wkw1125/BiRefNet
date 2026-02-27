80db67ba0d03e9c763c42ce28b0466cfbb87b605
wkw <wkw1125@163.com>
2026-02-27 20:38:08 +0800

    Revert "batch_size = 4"
    This reverts commit 4e99b9f69fff063c1b0fe38357f2069e4d30c777.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import math
 class Config():
     def __init__(self) -> None:
         # Main active settings
-        self.batch_size = 4                                     # Multi-GPU+BF16 training for 76GB / 62GB, without/with compile, on each A100.
+        self.batch_size = 2                                     # Multi-GPU+BF16 training for 76GB / 62GB, without/with compile, on each A100.
         self.compile = False                                    # 1. PyTorch<=2.0.1 has an inherent CPU memory leak problem; 2.0.1<PyTorch<2.5.0 cannot successfully compile.
         self.mixed_precision = ['no', 'fp16', 'bf16', 'fp8'][2] # 2. FP8 doesn't show acceleration in the torch.compile mode.
         self.SDPA_enabled = True                                # H200x1 + compile==True.  None: 43GB + 14s, math: 43GB + 15s, mem_eff: 35GB + 15s.
